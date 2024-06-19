0f24285636c2b2869e9c8801c5d4dde5c19996ce
mreza-jalali <155071737+mreza-jalali@users.noreply.github.com>
2024-06-19 17:21:21 +0330

    $COMMIT_MESSAGE

--- auto-commit.bat
+++ auto-commit.bat
@@ -1,3 +1,14 @@
+@echo off
+
+rem تنظیم متغیرها
+set source_url=http://sub.asdc.ir/0e845ece-7ed6-11ee-9744-9378b10b78a6
+set dest_file=C:\Users\jalali-m\Documents\GitHub\sub\jsub
+
+rem دانلود محتوای فایل
+curl -s %source_url% > %dest_file%
+
+
+
 # تغییرات را اضافه کنید
 git add .
 
